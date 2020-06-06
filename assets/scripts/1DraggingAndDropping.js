c32a329f2f43bc502896a133760d861209300655
alexander khazzam <alexanderkhazzam@gmail.com>
2020-06-06 12:25:53 -0400

    initial commit

---
+++ assets/scripts/1DraggingAndDropping.js
@@ -0,0 +1,17 @@
+//drag and drop theory
+/**
+ * mark the element as "draggable" to true in the html code
+ * list to a "dragstart" event; will be triggered when an element is dragged
+ * mark an area where it is supposed to be dropped as "dragenter" and "dragover"; can omit the "dragenter"
+ * must call prevent default because the default is that drag and drop doesn't work in the browser
+ * listen to a "dragleave" event when the item is dragged away from the element it was dragged on
+ * listen to drop event
+ * can listen to "Dragend" event when element is dropped in invalid area
+ */
+
+let mary = 12;
+let joe = 5;
+
+let blob = [1, 2, 3, 4, 5];
+
+blob.reduce((num) => u);
